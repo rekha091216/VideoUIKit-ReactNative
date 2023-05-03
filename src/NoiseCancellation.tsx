7e96a176ca3da485ee5ffe5be85346352c24bf9c
Rekha Babu <rekha_arjun@yahoo.com>
2023-05-03 11:18:36 +0530

    Update console logs

--- src/NoiseCancellation.tsx
+++ src/NoiseCancellation.tsx
@@ -9,7 +9,7 @@ import wasm1 from './../../node_modules/agora-extension-ai-denoiser/external/den
 import wasm2 from './../../node_modules/agora-extension-ai-denoiser/external/denoiser-wasm-simd.wasm';
 
 // Necessary To bypass treeshaking, dont remove
-console.log('wasm files loaded', wasm1, wasm2);
+console.log('wasm files loaded are', wasm1, wasm2);
 
 const NoiseCancellation: React.FC<{
   children: React.ReactNode;
