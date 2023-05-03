e5c1874c89b61d64ca8d3d48b6d1c50af7d8f3a7
Rekha Babu <rekha_arjun@yahoo.com>
2023-05-03 12:43:29 +0530

    Fix error while unpipe

--- src/NoiseCancellation.tsx
+++ src/NoiseCancellation.tsx
@@ -1,66 +1,70 @@
-import React, {useEffect, useRef} from 'react';
-import {RenderStateInterface} from './Contexts/RtcContext';
-import RtcEngine from 'react-native-agora';
-import AgoraRTC from 'agora-rtc-sdk-ng';
-import { AIDenoiserExtension, AIDenoiserProcessor, AIDenoiserProcessorMode, IAIDenoiserProcessor } from "agora-extension-ai-denoiser";
+import React, { useEffect, useRef } from "react";
+import { RenderStateInterface } from "./Contexts/RtcContext";
+import RtcEngine from "react-native-agora";
+import AgoraRTC from "agora-rtc-sdk-ng";
+import {
+  AIDenoiserExtension,
+  AIDenoiserProcessorMode,
+} from "agora-extension-ai-denoiser";
 //@ts-ignore
-import wasm1 from './../../node_modules/agora-extension-ai-denoiser/external/denoiser-wasm.wasm';
+import wasm1 from "./../../node_modules/agora-extension-ai-denoiser/external/denoiser-wasm.wasm";
 //@ts-ignore
-import wasm2 from './../../node_modules/agora-extension-ai-denoiser/external/denoiser-wasm-simd.wasm';
+import wasm2 from "./../../node_modules/agora-extension-ai-denoiser/external/denoiser-wasm-simd.wasm";
 
 // Necessary To bypass treeshaking, dont remove
-console.log('wasm files loaded are', wasm1, wasm2);
+console.log("wasm files loaded are", wasm1, wasm2);
 
 const NoiseCancellation: React.FC<{
   children: React.ReactNode;
   uidState: RenderStateInterface;
   engineRef: React.MutableRefObject<RtcEngine>;
-}> = ({engineRef, uidState, children}: any) => {
-
-  const {renderList, activeUids} = uidState;
+}> = ({ engineRef, uidState, children }: any) => {
+  
+  const { renderList, activeUids } = uidState;
   const [maxUid] = activeUids;
   const noiseSuppressionState = renderList[maxUid].noiseSuppression;
   const isAudioEnabled = renderList[maxUid].audio;
   let processor = useRef(null);
-  
+  const denoiserExtension = new AIDenoiserExtension({ assetsPath: "wasm" });
 
   useEffect(() => {
-    
-    if (isAudioEnabled) {
-      const denoiserExtension = new AIDenoiserExtension({ assetsPath: "wasm"});
+    const initExtension = async () => {
       AgoraRTC.registerExtensions([denoiserExtension]);
       processor.current = denoiserExtension.createProcessor();
-      
-      const localAudioTrack = engineRef.current?.localStream?.audio;
-      console.log('localaudiotrack' + localAudioTrack)
-      if (processor.current && localAudioTrack) {
-        localAudioTrack.pipe(processor.current).pipe(localAudioTrack.processorDestination);
-        processor.current.setMode(AIDenoiserProcessorMode.STATIONARY_NS);
-      }
-    }
-  },[isAudioEnabled])
-
+    };
+    initExtension();
+    enableNoiseSuppression();
+    return () => {
+      disableNoiseSuppression();
+    };
+  }, [isAudioEnabled]);
 
   useEffect(() => {
     if (noiseSuppressionState) {
       enableNoiseSuppression();
-    }
-    else
-    {
+    } else {
       disableNoiseSuppression();
     }
-   
   }, [noiseSuppressionState]);
 
   const enableNoiseSuppression = async () => {
-    console.log(processor.current)
+    const localAudioTrack = engineRef.current?.localStream?.audio;
+
+    if (processor.current && localAudioTrack) {
+      localAudioTrack
+        ?.pipe(processor.current)
+        .pipe(localAudioTrack.processorDestination);
+      processor.current?.setMode(AIDenoiserProcessorMode.STATIONARY_NS);
       await processor?.current?.enable();
     }
+  };
 
   const disableNoiseSuppression = async () => {
+    if (processor.current) {
+      processor.current.unpipe();
       await processor?.current?.disable();
-  }
-
+    }
+  };
   return <>{children}</>;
 };
 
